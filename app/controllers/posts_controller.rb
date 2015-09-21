d1f5ee971c6bead916f2be5c51879be0036119de
samkessaram <samkessaram@gmail.com>
2015-09-21 19:38:53 +0000

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,49 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id].to_i)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(
+      title:  params[:post][:title],
+      author: params[:post][:author],
+      text:   params[:post][:text]
+    )
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  protected
+
+  def post_params
+    params.require(:post).permit(:title, :author, :text)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'posts', to: 'posts#index'
-
-  get 'posts/:id', to: 'posts#show'
+  resources :posts
+  root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
